6ee5dbf5a834e0d0150aa47a2ac49eaad1d22707
Di Fan <26423504+dayfine@users.noreply.github.com>
2024-09-15 11:56:28 +0000

    QuickCheck

--- haskell/thebook/ch14/addition/Addition.hs
+++ haskell/thebook/ch14/addition/Addition.hs
@@ -1,6 +1,7 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
 
 sayHello :: IO ()
 sayHello = putStrLn "Hello!"
@@ -18,6 +19,8 @@ main = hspec $ do
             (1 + 1) > 1 `shouldBe` True
         it "2 + 2 is equal to 4" $ do
             2 + 2 `shouldBe` 4
+        it "x + 1 is always greater than x" $ do
+            property $ \x -> x + 1 > (x :: Int)
 
     describe "Division" $ do 
         it "15 divided by 3 is 5" $ do
