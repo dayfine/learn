95839e98b56e5334ce988ff179d8a5a8982e51d2
Docker <docker@example.com>
2024-12-07 09:55:53 +0000

    initial test

--- ocaml/more/lib/ch1.ml
+++ ocaml/more/lib/ch1.ml
@@ -1,4 +1,11 @@
 open Base
 
-let%test "rev" =
-  List.equal Int.equal (List.rev [ 3; 2; 1 ]) [ 1; 2; 3 ]
+
+let remove_expense_from_budget exs b = 
+  let f budget ex = budget - ex
+  in List.fold_left ~f:f ~init:b exs
+
+let%test_unit "remove_expense_from_budget 1" =
+  [%test_eq: int] (remove_expense_from_budget [1;2;3] 10) 4
+let%test_unit "remove_expense_from_budget 2" =
+  [%test_eq: int] (remove_expense_from_budget [3;2;1;2;3] 20) 9
